d1156e9abda42524ee6ce964dfaecbc0afecb02a
MohamedNasrr <mohamednasrrkh@gmail.com>
2024-09-11 00:51:35 +0300

    sliver scroll

---
+++ lib/features/home/presentation/views/widgets/book_seller_list_view.dart
@@ -0,0 +1,23 @@
+import 'package:bookly/features/home/presentation/views/widgets/best_seller_item.dart';
+import 'package:flutter/material.dart';
+
+class BookSellerListView extends StatelessWidget {
+  const BookSellerListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      physics: const NeverScrollableScrollPhysics(),
+      padding: EdgeInsets.zero,
+      itemCount: 10,
+      itemBuilder: (context, index) {
+        return const Padding(
+          padding: EdgeInsets.symmetric(
+            vertical: 10,
+          ),
+          child:  BestSellerListItem(),
+        );
+      },
+    );
+  }
+}
