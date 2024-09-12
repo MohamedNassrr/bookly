89042cf248cda3ef40debbb31e7f6d56fe806b3d
MohamedNasrr <mohamednasrrkh@gmail.com>
2024-09-13 01:38:02 +0300

    create search

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,5 +1,6 @@
 import 'package:bookly/features/home/presentation/views/books_details_view.dart';
 import 'package:bookly/features/home/presentation/views/home_view.dart';
+import 'package:bookly/features/search/presentation/views/search_view.dart';
 import 'package:bookly/features/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
@@ -8,12 +9,17 @@ abstract class AppRouter{
 
   static const kHomeView = '/homeView';
   static const booksDetailsView = '/BooksDetailsView';
+  static const kSearchView = '/SearchView';
 
   static final router = GoRouter(
     routes: [
       GoRoute(
         path: '/',
         builder: (BuildContext context, GoRouterState state) => const SplashView(),
+      ),
+      GoRoute(
+        path: kSearchView,
+        builder: (BuildContext context, GoRouterState state) => const SearchView(),
       ),
       GoRoute(
         path: kHomeView,
--- lib/features/home/presentation/views/widgets/home_widget/custom_app_bar.dart
+++ lib/features/home/presentation/views/widgets/home_widget/custom_app_bar.dart
@@ -1,6 +1,8 @@
+import 'package:bookly/core/utils/app_router.dart';
 import 'package:bookly/core/utils/assets_data.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomAppBar extends StatelessWidget {
   const CustomAppBar({super.key});
@@ -20,11 +22,13 @@ class CustomAppBar extends StatelessWidget {
           ),
           const Spacer(),
           IconButton(
-            onPressed: () {},
+            onPressed: () {
+              GoRouter.of(context).push(AppRouter.kSearchView);
+            },
             icon: const Icon(
               FontAwesomeIcons.magnifyingGlass,
               color: Colors.white,
-              size: 23,
+              size: 22,
             ),
           ),
         ],
