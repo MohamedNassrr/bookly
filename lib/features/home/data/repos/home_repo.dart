b0fc0aea4c9625c30f2c8f298457924bd88473f5
MohamedNasrr <mohamednasrrkh@gmail.com>
2024-09-16 00:59:55 +0300

    home repo methode and failures

---
+++ lib/features/home/data/repos/home_repo.dart
@@ -0,0 +1,8 @@
+import 'package:bookly/core/errors/failures.dart';
+import 'package:bookly/features/home/data/models/book_model/Book_model.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class HomeRepo {
+  Future<Either<Failures, List<BookModel>>> fetchBestSellerBooks();
+  Future<Either<Failures, List<BookModel>>> fetchFeaturedBooks();
+}
